3a255a2ee855e28b18ca34e5b8d56c7c01cbc616
KedGarci4 <kedwingarcia1@gmail.com>
2024-07-27 14:06:02 -0600

    no hay muchos cambios.

--- examen2ldp/lib/main.dart
+++ examen2ldp/lib/main.dart
@@ -3,11 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp( const MyApp());
 
 class MyApp extends StatelessWidget {
-
-
    const MyApp({super.key});
-  
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,7 +26,7 @@ class MyApp extends StatelessWidget {
                       icon: const Icon(Icons.send),
                       onPressed: () {
                         final baseURL = "https://yesno.wtf/api";
-                        
+
                       },
                       )
                   ),
